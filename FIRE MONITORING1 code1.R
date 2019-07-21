8b9b2b9cbfc211fd121e460b91903a497f0631cf
aryamb7 <52207109+aryamb7@users.noreply.github.com>
2019-07-21 12:54:36 -0400

    Add files via upload

--- FIRE MONITORING1 code1.R
+++ FIRE MONITORING1 code1.R
@@ -80,7 +80,102 @@ write_xlsx(x=testx,path="testx.xlsx",col_names = TRUE)
 write_xlsx(x=trainx,path="trainx.xlsx",col_names = TRUE)
 write_xlsx(x=x,path="x.xlsx",col_names = TRUE)
 
+sum(is.na(testx))
+sum(is.na(trainx))
+sum(is.na(x))
+
+
+#finding unique values in columns
+
+table(trainx$`Final Incident Type`)
+
+#bar plot vizualisation of Initial CAD Event type
+
+library(ggplot2)
+Incident <- table(trainx$`Initial CAD Event Type`)
+Incident
+barplot(Incident)
+
+# to put bars in descending order
+
+barplot(Incident[order(Incident, decreasing=TRUE)])
+barplot(Incident,col=rainbow(50))
+
+table(trainx$`Initial CAD Event Type`)
+
+cal<-table(testx$`Call Source`)
+cal
+barplot(cal)
+
+plot(x=trainx$'Incident Station Area',
+     y=trainx$'Incident Ward',
+     pch=19,cex=0.8,
+     frame=FALSE,
+     xlab="STATION AREA",
+     ylab="INCIDENT WARD")
+
+
+boxplot('Incident Ward'~'Incident Station Area',
+        data=x,
+        ylab="INCIDENT TYPE",
+        frame=FALSE,
+        col="lightgray")
+
+
+#barplotting for incident ward
+Incidentsw <- table(trainx$`Incident Ward`)
+Incidentsw
+barplot(Incidentsw)
+
+library(ggplot2)
+
+trainx <- within(trainx, 'Initial CAD Event Type' <- factor('Initial CAD Event Type', levels=names(sort(table('Initial CAD Event Type'), decreasing=TRUE))))
+qplot('Initial CAD Event Type', data = trainx, geom = "bar", fill = I("blue"))+
+  theme(axis.text.x=element_text(angle=90,hjust=1))
+
+
+#barplotting for incident station area
+Incidentsa <- table(trainx$`Incident Station Area`)
+Incidentsa
+trainx <- within(trainx, 'Incidentsa' <- factor('Incidentsa', levels=names(sort(table('Incidentsa'), decreasing=TRUE))))
+qplot('Incidentsa', data = trainx, geom = "bar", fill = I("blue"))+
+  theme(axis.text.x=element_text(angle=90,hjust=1))
+
+
+#barplot(Incidentsa)
+
+#barplotting for call source
+Incidentsc <- table(trainx$`Call Source`)
+Incidentsc
+barplot(Incidentsc)
+
+
+#barplotting for intersection
+Incidentsi <- table(trainx$`Intersection`)
+Incidentsi
+barplot(Incidentsi)
+
+
+#barplotting for Persons Rescued
+Incidentsp <- table(trainx$`Persons Rescued`)
+Incidentsp
+barplot(Incidentsp)
 
 
 
+
+#tableplot
+
+require(ggplot2)
+data(trainx)
+
+
+tableplot(trainx)
+tableplot(trainx, select = c(Final_Incident_Type,Initial_CAD_Event_Type,Incident_Station_Area,Incident_Ward), sortCol = Initial_CAD_Event_Type)
+
+
+ggplot(data=trainx)+geom_point(mapping=aes(x= 'Final Incident Type' , y= 'Persons Rescued'))
+library(ggplot2)
+ggplot(data=testx)+
+geom_point(mapping=aes(x= 'Final Incident Type' , y= 'Persons Rescued'))
 
